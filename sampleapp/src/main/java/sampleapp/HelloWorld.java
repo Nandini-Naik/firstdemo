dd8b4e6d1c28935f44481b6acf6d0cd5bba2b1d8
nandnaik <nandnaik@LIN25005230.corp.capgemini.com>
2021-08-17 10:58:50 +0530

    HelloWorld changes

--- sampleapp/src/main/java/sampleapp/HelloWorld.java
+++ sampleapp/src/main/java/sampleapp/HelloWorld.java
@@ -3,7 +3,10 @@ package sampleapp;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		System.out.println("Sum: " + sum);
 
 	}
 
